0281fd4120e708f173ad0f160f726bc4cf93d479
hsivov <hsivov@students.softuni.bg>
2024-10-13 19:01:30 +0300

    Add loggers

--- src/main/java/org/example/gamestoreapp/service/impl/EmailServiceImpl.java
+++ src/main/java/org/example/gamestoreapp/service/impl/EmailServiceImpl.java
@@ -3,6 +3,8 @@ package org.example.gamestoreapp.service.impl;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.example.gamestoreapp.service.EmailService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -10,21 +12,27 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmailServiceImpl implements EmailService {
     private final JavaMailSender mailSender;
+    private final static Logger logger = LoggerFactory.getLogger(EmailServiceImpl.class);
 
     public EmailServiceImpl(JavaMailSender mailSender) {
         this.mailSender = mailSender;
     }
 
     @Override
-    public void sendEmail(String to, String subject, String text) throws MessagingException {
-        MimeMessage message = mailSender.createMimeMessage();
+    public void sendEmail(String to, String subject, String text) {
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
 
-        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
 
-        helper.setTo(to);
-        helper.setSubject(subject);
-        helper.setText(text, true);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(text, true);
+            helper.setFrom("hsivov@gmail.com");
 
-        mailSender.send(message);
+            mailSender.send(message);
+        } catch (MessagingException e) {
+            logger.error("Failed to send confirmation email: {}", e.getMessage(), e);
+        }
     }
 }
