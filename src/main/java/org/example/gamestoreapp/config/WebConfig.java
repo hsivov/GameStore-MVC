34129cb070bfdcb3c23dbf91c78090f7f8b0fdc7
hsivov <hsivov@students.softuni.bg>
2024-12-30 18:15:14 +0200

    Internationalization configuration

--- src/main/java/org/example/gamestoreapp/config/WebConfig.java
+++ src/main/java/org/example/gamestoreapp/config/WebConfig.java
@@ -1,9 +1,11 @@
 package org.example.gamestoreapp.config;
 
 import org.example.gamestoreapp.interceptor.HMACInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -13,9 +15,19 @@ public class WebConfig implements WebMvcConfigurer {
         this.hmacInterceptor = hmacInterceptor;
     }
 
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("lang");
+        return interceptor;
+    }
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(hmacInterceptor)
+        registry
+                .addInterceptor(hmacInterceptor)
                 .addPathPatterns("/api/**");
+
+        registry.addInterceptor(localeChangeInterceptor());
     }
 }
