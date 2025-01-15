7a4103795de33851c13ff7ac20d77b60665a39e6
hsivov <hsivov@students.softuni.bg>
2025-01-15 20:05:06 +0200

    Add footer, add contacts template

--- src/main/java/org/example/gamestoreapp/config/SecurityConfig.java
+++ src/main/java/org/example/gamestoreapp/config/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig {
                             authorizeRequest
                                     .requestMatchers("/admin/**").hasRole("ADMIN")
                                     .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                                    .requestMatchers("/", "/store/**", "/api/**", "/error/**").permitAll()
+                                    .requestMatchers("/", "/contacts", "/store/**", "/api/**", "/error/**").permitAll()
                                     .requestMatchers("/auth/**").anonymous()
                                     .anyRequest().authenticated();
                         }
